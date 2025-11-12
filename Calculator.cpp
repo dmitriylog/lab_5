0c418cebf712c7d3b6ae0b5e3421a7f634f5b805
d1m1d1n <dmitriy-loginov-02@mail.ru>
2025-11-12 16:50:03 +0300

    first test program (not working)

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,7 +1,110 @@
 #include <iostream>
+#include <QApplication>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QStackedWidget>
 
 using namespace std;
 
-int main() {
-  cout << "this is calculator";
+// класс калькулятора с т.з. графической составляющей
+class Calculator 
+{
+    Q_OBJECT
+  public:
+                        // параметр указывающий на родительский виджет, nullptr - значение по умолчанию
+    Calculator(QWidget *parent = nullptr) : QWidget(parent) // вызывает конструктор базового класса QWidget и передает ему параметр parent
+    {
+      switcherWidget = new QStackedWidget(this); // переключатель между калькулятором и историей
+      
+      calculatorWidget = new QWidget;
+      historyWidget = new QWidget;
+      
+      setupCalculatorUI(); // настройка интерфейсов
+      setupHistoryUI();  
+      
+      // добавление виджетов калькулятора и истории в свитчер
+      switcherWidget->addWidget(calculatorWidget);
+      switcherWidget->addWidget(historyWidget);
+      
+      QVBoxLayout *mainLayout = new QVBoxLayout(this); // для текущего объекта, mainLayout - вертикальное расположение для главного окна калькулятора
+      mainLayot->addWidget(switcherWidget); // занимает всё место в окне
+      
+      switcherWidget->setCurrentIndex(0); // по умолчанию показывается калькулятор
+      
+    }
+  
+  private slots:
+    void onDigitClicked()
+    {
+    
+    }
+    
+    void on_Operator_Clicked()
+    {
+    
+    }
+    
+    void onEqualsClicked()
+    {
+    
+    }
+    
+    void onClearClicked()
+    {
+    
+    }
+    
+    void onBackspaceClicked()
+    {
+    
+    }
+    
+    void on_ClearHistory_Clicked()
+    {
+    
+    }
+    
+    void showHistory()
+    {
+    
+    }
+    
+    void showCalculator()
+    {
+    
+    }
+    
+  private:
+    void setupCalculatorUI()
+    {
+     
+    
+    
+    
+    }
+  
+    void setupHistoryUI()
+    {
+  
+    }
+  
+    void add_to_History()
+    {
+  
+    }
+  
+  
+
 }
+
+
+
+
+
+
+
+int main() {
+
+}
+
+#include "Calculator.moc"
