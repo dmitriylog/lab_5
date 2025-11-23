76d8a787158400dc15145e74140b1d33b9649ea8
Valerka <Valerka@gmail.com>
2025-11-23 22:13:46 +0300

    Функция возведения в степень теперь точно

---
+++ Lab5(Degree).cpp
@@ -0,0 +1,35 @@
+﻿/*
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Операция возведения в любую степень (не только квадрат) 
+//                  4          5
+
+*/
+
+float Degree(float num, float deg) {
+	int k = 1; float sum = num;
+	while (k < deg) {
+		sum *= num;
+		k += 1;
+	}
+	return sum;
+}
+
+/*
+* 
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	float a, b;
+	cout << "Input number: "; cin >> a;// cout << endl;
+	cout << "Input degree: "; cin >> b;// cout << endl;
+	cout << Degree(a, b);
+
+	return 0;
+} 
+
+*/
