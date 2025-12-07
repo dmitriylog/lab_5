24d57a63dc8a483276c91458e012666787410dd9
Давид Малаев <d-malaev@inbox.ru>
2025-12-07 22:32:24 +0300

    Добавлен котангенс Малаев

--- Calculator.cpp
+++ Calculator.cpp
@@ -112,7 +112,7 @@ float cotangent_deg(float degrees) {
     float mod = fmod(fabs(degrees), 180.0f);
     
     if (fabs(mod) < 1e-6) {
-        throw std::runtime_error("Котангенс не определён при углах 0 + 180*k градусов!");
+        throw std::runtime_error("Котангенс не определён при углах 0 + 180*k градусов! ");
     }
     
     float tan_value = tan(radians);
